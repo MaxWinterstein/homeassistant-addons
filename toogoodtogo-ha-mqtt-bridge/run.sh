3a26d0c36a2fbfb512a490655540425d14386324
Max Winterstein <MaxWinterstein@users.noreply.github.com>
2021-12-07 12:56:43 +0100

    Optimize image

--- toogoodtogo-ha-mqtt-bridge/run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bashio
-
-bashio::log.info "Copy setings file"
-cp /data/options.json /app/toogoodtogo_ha_mqtt_bridge/settings.local.json
-
-bashio::log.info "Strating Bridge"
-python toogoodtogo_ha_mqtt_bridge/main.py
